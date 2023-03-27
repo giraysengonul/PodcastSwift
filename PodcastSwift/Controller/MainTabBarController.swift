e9e28640c60e2835a883bf0287465f03c5afe073
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-03-27 10:16:14 +0300

    Setup

---
+++ PodcastSwift/Controller/MainTabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  MainTabBarController.swift
+//  PodcastSwift
+//
+//  Created by hakkı can şengönül on 27.03.2023.
+//
+
+import UIKit
+class MainTabBarController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBrown
+    }
+}
