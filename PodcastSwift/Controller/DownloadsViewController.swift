5b28bd328c48633605f828a0d6d9e54bff3cb933
Hakkı Can Şengönül <coder.9734@gmail.com>
2023-05-01 14:21:42 +0300

    DownloadsViewController Setup

--- PodcastSwift/Controller/DownloadsViewController.swift
+++ PodcastSwift/Controller/DownloadsViewController.swift
@@ -6,21 +6,35 @@
 //
 
 import UIKit
-class DownloadsViewController: UIViewController {
+private let reuseIdentifiew = "DownloadCell"
+class DownloadsViewController: UITableViewController {
      // MARK: - Properties
      // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        style()
-        layout()
+        setup()
     }
 }
  // MARK: - Helpers
 extension DownloadsViewController{
-    private func style(){
-        view.backgroundColor = .red
+    private func setup(){
+        view.backgroundColor = .white
+        tableView.register(EpisodeCell.self, forCellReuseIdentifier: reuseIdentifiew)
     }
-    private func layout(){
-        
+}
+ // MARK: - UITableViewDataSource
+extension DownloadsViewController{
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifiew, for: indexPath) as! EpisodeCell
+        return cell
+    }
+}
+ // MARK: - UITableViewDelegate
+extension DownloadsViewController{
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 134
     }
 }
